29b1302d71728de35798443d42d6304a4141aa77
Vincent Torri <vincent.torri@gmail.com>
2012-02-11 10:18:33 +0000

    Etch: autotools improvements

--- src/include/Etch.h
+++ src/include/Etch.h
@@ -18,7 +18,33 @@
 #ifndef ETCH_H_
 #define ETCH_H_
 
-#include "Eina.h"
+#include <Eina.h>
+
+#ifdef EAPI
+# undef EAPI
+#endif
+
+#ifdef _WIN32
+# ifdef EFL_ETCH_BUILD
+#  ifdef DLL_EXPORT
+#   define EAPI __declspec(dllexport)
+#  else
+#   define EAPI
+#  endif /* ! DLL_EXPORT */
+# else
+#  define EAPI __declspec(dllimport)
+# endif /* ! EFL_ETCH_BUILD */
+#else
+# ifdef __GNUC__
+#  if __GNUC__ >= 4
+#   define EAPI __attribute__ ((visibility("default")))
+#  else
+#   define EAPI
+#  endif
+# else
+#  define EAPI
+# endif
+#endif
 
 /**
  * @mainpage Etch
